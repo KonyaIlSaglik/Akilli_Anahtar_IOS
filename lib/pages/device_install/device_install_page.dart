a7e4698800d1db6087205a8184700c979982fc99
mehmetcekici <mehmetcekici427@gmail.com>
2024-07-24 16:47:37 +0300

    24.07.2024

--- lib/pages/device_install/device_install_page.dart
+++ lib/pages/device_install/device_install_page.dart
@@ -1,5 +1,7 @@
+import 'package:akilli_anahtar/pages/home/home_page.dart';
 import 'package:akilli_anahtar/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import 'package:wifi_iot/wifi_iot.dart';
 import 'package:http/http.dart' as http;
 import 'package:wifi_scan/wifi_scan.dart';
@@ -12,6 +14,7 @@ class DeviceInstallPage extends StatefulWidget {
 }
 
 class _DeviceInstallPageState extends State<DeviceInstallPage> {
+  final controller = PageController(viewportFraction: 0.8, keepPage: true);
   bool isConnected = false;
   bool scanning = true;
   List<WiFiAccessPoint> accessPoints = <WiFiAccessPoint>[];
@@ -23,45 +26,85 @@ class _DeviceInstallPageState extends State<DeviceInstallPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: goldColor,
-        actions: [
-          IconButton(
-            onPressed: () {
-              startScan();
-            },
-            icon: Icon(
-              Icons.refresh_outlined,
-            ),
-          )
-        ],
-      ),
-      body: scanning
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : accessPoints.isEmpty
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          //image: Icon(Icons.done_outlined, size: 50),
+          decoration: PageDecoration(
+            titlePadding: EdgeInsets.symmetric(vertical: 25),
+            footerFlex: 10,
+            bodyFlex: 100,
+          ),
+          // footer: Center(
+          //     child:
+          //         Text("Listeden Akıllı Anahtar cihazınızı seçip bağlanın.")),
+          image: isConnected ? Icon(Icons.done) : null,
+          title: "Cihazınızı Bağlayın.",
+          bodyWidget: scanning
               ? Center(
-                  child: Text("Wifi bulunamadı."),
+                  child: CircularProgressIndicator(),
                 )
-              : ListView.separated(
-                  separatorBuilder: (BuildContext context, int index) =>
-                      const Divider(),
-                  itemCount: accessPoints.length,
-                  itemBuilder: ((context, i) {
-                    return AccessPointTile(accessPoint: accessPoints[i]);
-                    // return Padding(
-                    //   padding: const EdgeInsets.symmetric(horizontal: 10),
-                    //   child: ListTile(
-                    //     leading: Icon(Icons.wifi),
-                    //     title: Text(accessPoints[i].ssid),
-                    //     subtitle: Text(accessPoints[i].capabilities),
-                    //     trailing: Icon(Icons.chevron_right_outlined),
-                    //   ),
-                    // );
-                  }),
-                ),
+              : accessPoints.isEmpty
+                  ? Center(
+                      child: Text("Wifi bulunamadı."),
+                    )
+                  : Card(
+                      color: goldColor,
+                      elevation: 5,
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.90,
+                        height: MediaQuery.of(context).size.height * 0.70,
+                        child: ListView.separated(
+                          separatorBuilder: (BuildContext context, int index) =>
+                              const Divider(),
+                          itemCount: accessPoints.length,
+                          itemBuilder: ((context, i) {
+                            return AccessPointTile(
+                                accessPoint: accessPoints[i]);
+                            // return Padding(
+                            //   padding: const EdgeInsets.symmetric(horizontal: 10),
+                            //   child: ListTile(
+                            //     leading: Icon(Icons.wifi),
+                            //     title: Text(accessPoints[i].ssid),
+                            //     subtitle: Text(accessPoints[i].capabilities),
+                            //     trailing: Icon(Icons.chevron_right_outlined),
+                            //   ),
+                            // );
+                          }),
+                        ),
+                      ),
+                    ),
+        ),
+        PageViewModel(
+          title: "Title of introduction page",
+          body: "Welcome to the app! This is a description of how it works.",
+          image: const Center(
+            child: Icon(Icons.waving_hand, size: 50.0),
+          ),
+        ),
+        PageViewModel(
+          title: "Title of introduction page",
+          body: "Welcome to the app! This is a description of how it works.",
+          image: const Center(
+            child: Icon(Icons.waving_hand, size: 50.0),
+          ),
+        ),
+      ],
+      controlsPadding: EdgeInsets.only(top: 50),
+      showNextButton: true,
+      next: Text("Sonraki"),
+      //showBackButton: true,
+      //back: Text("Önceki"),
+      showSkipButton: true,
+      skip: Text("Geç"),
+      done: const Text("Tamamlandı"),
+      onDone: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ),
+        );
+      },
     );
   }
 
@@ -84,7 +127,6 @@ class _DeviceInstallPageState extends State<DeviceInstallPage> {
 
 class AccessPointTile extends StatelessWidget {
   final WiFiAccessPoint accessPoint;
-
   const AccessPointTile({Key? key, required this.accessPoint})
       : super(key: key);
 
@@ -107,16 +149,28 @@ class AccessPointTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final title = accessPoint.ssid.isNotEmpty ? accessPoint.ssid : "**EMPTY**";
+    const color = Colors.white;
     final signalIcon = accessPoint.level >= -80
         ? Icons.wifi_outlined
         : Icons.wifi_2_bar_outlined;
     return ListTile(
       visualDensity: VisualDensity.compact,
-      leading: Icon(signalIcon),
-      title: Text(title),
-      subtitle: Text(accessPoint.capabilities),
+      leading: Icon(signalIcon, color: color),
+      title: Text(
+        title,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(color: color),
+      ),
+      subtitle: Text(
+        accessPoint.capabilities,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(color: color),
+      ),
       trailing: IconButton(
-        icon: Icon(Icons.info_outline),
+        icon: Icon(
+          Icons.info_outline,
+          color: color,
+        ),
         onPressed: () => showDialog(
           context: context,
           builder: (context) => AlertDialog(
@@ -158,10 +212,9 @@ class AccessPointTile extends StatelessWidget {
                 onPressed: () {
                   WiFiForIoTPlugin.isEnabled().then((value) {
                     if (value) {
-                      WiFiForIoTPlugin.findAndConnect(title,
-                              password: "AA123456")
+                      WiFiForIoTPlugin.connect(title, password: "AA123456")
                           .then((value) {
-                        print("sgseghsegse$value");
+                        Navigator.pop(context);
                         // if (value) {
                         //   print("parametreler gonderiliyor");
                         //   var uri = Uri.parse(
@@ -183,3 +236,48 @@ class AccessPointTile extends StatelessWidget {
     );
   }
 }
+
+/*
+
+Scaffold(
+      appBar: AppBar(
+        backgroundColor: goldColor,
+        actions: [
+          IconButton(
+            onPressed: () {
+              startScan();
+            },
+            icon: Icon(
+              Icons.refresh_outlined,
+            ),
+          )
+        ],
+      ),
+      body: scanning
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : accessPoints.isEmpty
+              ? Center(
+                  child: Text("Wifi bulunamadı."),
+                )
+              : ListView.separated(
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const Divider(),
+                  itemCount: accessPoints.length,
+                  itemBuilder: ((context, i) {
+                    return AccessPointTile(accessPoint: accessPoints[i]);
+                    // return Padding(
+                    //   padding: const EdgeInsets.symmetric(horizontal: 10),
+                    //   child: ListTile(
+                    //     leading: Icon(Icons.wifi),
+                    //     title: Text(accessPoints[i].ssid),
+                    //     subtitle: Text(accessPoints[i].capabilities),
+                    //     trailing: Icon(Icons.chevron_right_outlined),
+                    //   ),
+                    // );
+                  }),
+                ),
+    );
+
+*/
