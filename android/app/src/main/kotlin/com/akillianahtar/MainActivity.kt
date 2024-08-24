0da61b8dab8dcb7ce07ecf7dee7663531353eca6
mehmetcekici <mehmetcekici427@gmail.com>
2024-08-24 15:44:35 +0300

    24.08.2024

--- android/app/src/main/kotlin/com/akillianahtar/MainActivity.kt
+++ android/app/src/main/kotlin/com/akillianahtar/MainActivity.kt
@@ -8,25 +8,9 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
-    private val CHANNEL = "com.example.wifi"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
-            .setMethodCallHandler { call, result ->
-                if (call.method == "getRouterIP") {
-                    val ip = getRouterIP()
-                    result.success(ip)
-                } else {
-                    result.notImplemented()
-                }
-            }
-    }
-
-    private fun getRouterIP(): String? {
-        val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        val dhcpInfo = wifiManager.dhcpInfo
-        val ipAddress = dhcpInfo.gateway
-        return ipAddress.toString()
+        
     }
 }
